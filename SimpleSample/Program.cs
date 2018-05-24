d592d561259576e97a309d00b49bf47fff31e602
Edward Aschan <edward@aschan.org>
2018-05-24 11:48:17 +0200

    Initial commit

---
+++ SimpleSample/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleSample
+{
+    using System;
+
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
